5ef8872c70a48c5b1bfa8077d817ab5d4b45681d
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2017-01-24 12:33:03 -0800

    refactor added features, styling

--- app.js
+++ app.js
@@ -5,29 +5,48 @@ class TopComponent extends React.Component {
     this.state = {
       message: 'Default',
       size: 20,
-      className: 'classic'
+      className: 'classic',
+      lengthError: false
     }
+
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(e) {
-    let {name, value} = e.target
+    let { name, value } = e.target
+
+    if (name === 'message' && value.length > 16) {
+      e.target.value = value.slice(0, 16)
+      return this.setState({
+        lengthError: true,
+        message: value.slice(0, 16)
+      })
+    }
+
     value = name === 'size' ? Number(value) : value
-    this.setState({[name]: value})
+    this.setState({ [name]: value, lengthError: false })
   }
 
   render() {
+    const { message, size, className, lengthError } = this.state
+    const marginTop = Math.floor(40 - (size / 2))
     const options = ['classic', 'modern']
+
     return (
-      <div>
+      <div className="editor">
         <form onChange={this.handleChange}>
-          <input type="text" name="message" onChange={this.handleTextInput} autoComplete="off"></input>
-          <input type="range" name="size" max="40" min="15" onChange={this.handleSlider}></input>
-          <select name="className" onChange={this.handleSelect}>
+          <span style={{ display: `${lengthError ? 'inline' : 'none'}`}}>
+            Max 16 Characters
+          </span>
+          <input className={lengthError && 'error'} type="text" name="message" autoComplete="off" />
+          <input type="range" name="size" max="40" min="10" />
+          <select name="className" >
             {options.map((c, i) => (<option key={i} value={c}>{c}</option>))}
           </select>
         </form>
-        <h1 className={this.state.className} style={{fontSize: this.state.size}}>{this.state.message}</h1>
+        <h1 className={className} style={{ fontSize: size, marginTop }}>
+          {message}
+        </h1>
       </div>
     )
   }
